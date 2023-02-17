c3fc16766ac7f52b9db1482fd1236f935b53cfe1
GUEVARA0331 <jdguevara01@misena.edu.co>
2023-02-16 21:20:43 -0500

    Widget apilando textos

--- lib/review.dart
+++ lib/review.dart
@@ -1,12 +1,74 @@
+// ignore_for_file: prefer_const_constructors, must_be_immutable
+
 import 'package:flutter/material.dart'; 
 
 class Review extends StatelessWidget {
-  const Review({super.key});
 
   String pathImage = "asset/img/avatar-9d3a3ad47bb31dfc0fa90e7208828734.jpg"; 
+  String name = "Varuna Yasas"; 
+  String details = "1 review 5 photos"; 
+  String comment = "There is an amazing place in Sri Lanka";
+
+  Review({super.key}); 
 
   @override
   Widget build(BuildContext context) {
+
+    final userComment = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: "Lato", 
+          fontSize: 13.0, 
+          fontWeight: FontWeight.w900
+        ),
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: "Lato", 
+          fontSize: 13.0,
+          color: Color(0xFFa3a5a7)
+        ),
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20.0,
+      ),
+
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontSize: 17.0
+        ),
+      ),
+    ); 
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start, /* Alinear hacía la izquierda */
+      children: <Widget>[
+        userName,
+        userInfo, 
+        userComment
+      ],
+    ); 
 
     final photo = Container(
       margin: EdgeInsets.only(
@@ -28,7 +90,8 @@ class Review extends StatelessWidget {
 
     return Row(
       children: <Widget>[
-
+        photo, 
+        userDetails
       ],
     );
   }
